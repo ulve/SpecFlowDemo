52d492389802633bba6b43d61b3cb8f4e7cfe100
Olov Johansson <johansson.olov@gmail.com>
2015-01-31 13:37:48 +0100

    Nu vet jag inte

---
+++ Acceptanstest/Steps/TidrapporteraSteps.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Acceptanstest.Steps
+{
+    [Binding]
+    public class TidrapporteraSteps
+    {
+        [Given(@"jag har fyllt i tiden (.*) timmar")]
+        public void GivetJagHarFylltITidenTimmar(string tid)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"jag har fyllt i kunden (.*)")]
+        public void GivetJagHarFylltIKundenNissesKottOchTV(string kund)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"jag trycker på lägg till")]
+        public void NarJagTryckerPaLaggTill()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"skall en ny post läggast till")]
+        public void SaSkallEnNyPostLaggastTill()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"jag har fyllt följande värden (.*), (.*) och (.*)")]
+        public void GivetJagHarFylltFoljandeVardenNissesKottOchTVJagVetInteHurLangeJagJobbat(string tid, string kund, string kommentar)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"skall jag få (.*)")]
+        public void SaSkallJagFaTidSaknas(string meddelande)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
---
+++ Acceptanstest/Tidrapportera.feature
@@ -0,0 +1,24 @@
+﻿Egenskap: Tidrapportera
+	För att kunna fakturera kunder
+	Vill jag som anställd på Agio
+	Kunna bokföra mina jobbade timmar
+	
+Bakgrund: Givet att jag befinner mig på inmatningssidan
+
+Scenario: Bokföra timme
+	Givet jag har fyllt i tiden 3 timmar 
+	Och jag har fyllt i kunden Nisses kött och TV 
+	När jag trycker på lägg till
+	Så skall en ny post läggast till
+
+Scenariomall: Alla obligatoriska fält måste vara ifyllda
+	Givet jag har fyllt följande värden <tid>, <kund> och <kommentar>
+	När jag trycker på lägg till
+	Så skall jag få <meddelande>
+
+	Exempel: 
+	| tid | kund               | kommentar                         | meddelande          |
+	|     | Nisses kött och TV | Jag vet inte hur länge jag jobbat | Tid saknas          |
+	| 3   |                    | Vem jobbade jag nu åt			   | Kund saknas         |
+	|     |                    | ÖÖh vad har jag nu gjort		   | Tid och kund saknas |
+
